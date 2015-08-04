3ed74f43ca73979b79e7507782ee4169acee1f82
Rizart Dokollari <r.dokollari@gmail.com>
2015-08-04 17:36:05 +0000

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,6 @@
+## PetMall CMS
+A CMS and e-shop for a pet shop firm.
+
+### License
+
+The PetMall is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ tests/functional/public_user/HomeTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author  Rizart Dokollari <r.dokollari@gmail.com>
+ * @since   4/8/2015
+ */
+
+namespace tests\functional\public_user;
+
+
+use TestCase;
+
+class HomeTest extends TestCase
+{
+
+	/** @test */
+	public function it_reads_home_index()
+	{
+	}
+}
