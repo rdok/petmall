0dbe2509159f8a9578686a2c1d76285c5a491ce1
Rizart Dokollari <r.dokollari@gmail.com>
2015-09-12 02:58:13 +0300

    Add google map to footer, and increase height.

--- resources/views/e_shop/_partials/footer.blade.php
+++ resources/views/e_shop/_partials/footer.blade.php
@@ -1,38 +1,42 @@
 <div class="row" id="footer">
     <div class="container">
-        <footer class="footer">
-            <div class="hidden-xs col-sm-3 col-md-2" id="gooogle-map">
-                <div id="googleMap" style="width: 140px; height: 150px;"></div>
-            </div>
-            <div class="col-md-4 col-sm-3 col-md-push-1">
-                <h4>Γενικά</h4>
-                <ul class="list-group">
-                    <li class="list-group-item">Όροι χρήσης</li>
-                    <li class="list-group-item">Τρόποι Πληρωμής</li>
-                    <li class="list-group-item">Τρόποι αποστολής</li>
-                    <li class="list-group-item">Προστασία Προσωπικών Δεδομένων</li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-sm-3">
-                <h4>Κατάστημα</h4>
-                <ul class="list-group">
-                    <li class="list-group-item">Περικλέους 42, Μαρούσι, Αθήνα</li>
-                    <li class="list-group-item">ΤΗΛ.: 2106140670</li>
-                    <li class="list-group-item">FAX: 210802672</li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-sm-3 pull-right" id="news-letter">
-                <div class="pull-right">
-                    <h4>Newseletter</h4>
-                    <ul class="list-group pull-right">
-                        <li class="list-group-item">
-                            <input type="email" name="email" placeholder="e-mail"/>
-                        </li>
-                        <li class="list-group-item">Βρείτε μας στο Facebook</li>
-                    </ul>
-
+        <div class="row">
+            <footer class="footer">
+                <div class="hidden-xs col-sm-3 col-md-2" id="gooogle-map">
+                    <div id="gmap">
+                        <iframe width="200" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=%CE%A0%CE%B5%CF%81%CE%B9%CE%BA%CE%BB%CE%AD%CE%BF%CF%85%CF%82+42,+%CE%9C%CE%B1%CF%81%CE%BF%CF%8D%CF%83%CE%B9&amp;oe=utf-8&amp;client=firefox-a&amp;ie=UTF8&amp;hq=&amp;hnear=Perikleous+42,+Marousi+15122,+Athina,+Greece&amp;ll=38.059716,23.798103&amp;spn=0.054064,0.068321&amp;z=12&amp;iwloc=A&amp;output=embed"></iframe>
+                    </div>
                 </div>
-            </div>
-        </footer>
+                <div class="col-md-4 col-sm-3 col-md-push-1">
+                    <h4>Γενικά</h4>
+                    <ul class="list-group">
+                        <li class="list-group-item">Όροι χρήσης</li>
+                        <li class="list-group-item">Τρόποι Πληρωμής</li>
+                        <li class="list-group-item">Τρόποι αποστολής</li>
+                        <li class="list-group-item">Προστασία Προσωπικών Δεδομένων</li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h4>Κατάστημα</h4>
+                    <ul class="list-group">
+                        <li class="list-group-item">Περικλέους 42, Μαρούσι, Αθήνα</li>
+                        <li class="list-group-item">ΤΗΛ.: 2106140670</li>
+                        <li class="list-group-item">FAX: 210802672</li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3 pull-right" id="news-letter">
+                    <div class="pull-right">
+                        <h4>Newseletter</h4>
+                        <ul class="list-group pull-right">
+                            <li class="list-group-item">
+                                <input type="email" name="email" placeholder="e-mail"/>
+                            </li>
+                            <li class="list-group-item">Βρείτε μας στο Facebook</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </footer>
+        </div>
     </div>
 </div>
--- resources/views/e_shop/layouts/master.blade.php
+++ resources/views/e_shop/layouts/master.blade.php
@@ -50,6 +50,12 @@
     $(function () {
         $('#main-menu').smartmenus();
         $('#flash-overlay-modal').modal();
+        var mapOptions = {
+            center: new google.maps.LatLng(51.503454, -0.119562),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
     });
 </script>
 
